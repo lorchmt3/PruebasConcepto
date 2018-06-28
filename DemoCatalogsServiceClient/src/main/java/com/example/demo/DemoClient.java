33a5a3317fbcca57e996d23aa7bb212492846d2d
Diana Lorena Martinez <dianal.martinezt@javeriana.edu.co>
2018-06-28 16:50:18 -0500

    prueba concepto de creación cliente para consumo de servicio

---
+++ DemoCatalogsServiceClient/src/main/java/com/example/demo/DemoClient.java
@@ -0,0 +1,71 @@
+/**
+ * 
+ */
+package com.example.demo;
+
+import java.util.List;
+
+
+import org.apache.cxf.jaxws.JaxWsProxyFactoryBean;
+
+import com.grupoaval.payments.v1.FinancialEntitiesInq;
+import com.grupoaval.payments.v1.GetFinancialEntitiesRequest;
+import com.grupoaval.payments.v1.GetFinancialEntitiesResponse;
+import com.grupoaval.payments.v1.GetFinancialEntitiesRqType;
+import com.grupoaval.xsd.ifx.AdditionalDataType;
+import com.grupoaval.xsd.ifx.CatalogueType;
+
+/**
+ * @author diana.martinez
+ *
+ */
+
+public class DemoClient {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// https://192.168.135.28:444/PJBA_Multiaplicacion-consultarDatosCatalogos/FinancialEntitiesInqPort
+//		String serviceUrl = "http://localhost:8084/catalog/financialEntities?WSDL";// 192.168.135.28 ---- 10.5.14.39
+		String serviceUrl = "https://192.168.135.28:442/PJBA_Multiaplicacion-consultarDatosCatalogos/FinancialEntitiesInqPort?wsdl";// 192.168.135.28 ---- 10.5.14.39
+		// String serviceUrl =
+		// "https://192.168.135.28:444/PJBA_Multiaplicacion-consultarDatosCatalogos/FinancialEntitiesInqPort?WSDL";//192.168.135.28
+		// ---- 10.5.14.39
+		JaxWsProxyFactoryBean factory = new JaxWsProxyFactoryBean();
+		factory.setServiceClass(FinancialEntitiesInq.class);
+		factory.setAddress(serviceUrl);
+		FinancialEntitiesInq catalogsTypesProxy = (FinancialEntitiesInq) factory.create();
+
+		GetFinancialEntitiesRequest getFinancialEntitiesRq = new GetFinancialEntitiesRequest();
+		GetFinancialEntitiesRqType request = new GetFinancialEntitiesRqType();
+		List<CatalogueType> catalogueList = request.getCatalogue();
+
+		CatalogueType catalogue = new CatalogueType();
+		AdditionalDataType additionalData = new AdditionalDataType();
+		additionalData.setValue("GE");
+		catalogue.setAdditionalData(additionalData);
+		catalogue.setKey("100");
+		catalogueList.add(catalogue);
+
+		// RefInfoType refInfoType = new RefInfoType();
+		// refInfoType.setRefId("prprueba");
+		// refInfoType.setRefType("prueba");
+		// request.setRefInfo(refInfoType);
+		
+		getFinancialEntitiesRq.setGetFinancialEntitiesRq(request);
+		System.out.println("mi catalogo size:"+request.getCatalogue().size());
+		GetFinancialEntitiesResponse response = catalogsTypesProxy.getFinancialEntities(getFinancialEntitiesRq);
+
+		System.out.println(response.toString());
+		List<CatalogueType> catalogList = response.getGetFinancialEntitiesRs().getCatalogue();
+		for (CatalogueType catalog : catalogList) {
+			System.out.println("Catalog key - " + catalog.getKey() + " - AddionalData - "+catalog.getAdditionalData().getValue());
+			// }
+//			System.out.println("ApprovalId - " + response.getGetFinancialEntitiesRs().getApprovalId());
+
+		}
+
+		// http://192.168.135.28:442/PJBA_Multiaplicacion-consultarDatosCatalogos/FinancialEntitiesInqPort
+	}
+}
