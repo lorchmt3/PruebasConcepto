7e93deb3b3867d02068725cffbaca1f858af77fc
Diana Lorena Martinez <dianal.martinezt@javeriana.edu.co>
2018-07-03 08:19:24 -0500

    actualizacion con certificado SSL

--- DemoCatalogsServiceClient/src/main/java/com/example/demo/DemoClient.java
+++ DemoCatalogsServiceClient/src/main/java/com/example/demo/DemoClient.java
@@ -3,10 +3,22 @@
  */
 package com.example.demo;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.net.URL;
+import java.security.KeyStore;
 import java.util.List;
 
+import javax.net.ssl.KeyManager;
+import javax.net.ssl.KeyManagerFactory;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.TrustManagerFactory;
 
+import org.apache.cxf.configuration.jsse.TLSClientParameters;
+import org.apache.cxf.endpoint.Client;
+import org.apache.cxf.frontend.ClientProxy;
 import org.apache.cxf.jaxws.JaxWsProxyFactoryBean;
+import org.apache.cxf.transport.http.HTTPConduit;
 
 import com.grupoaval.payments.v1.FinancialEntitiesInq;
 import com.grupoaval.payments.v1.GetFinancialEntitiesRequest;
@@ -14,6 +26,7 @@ import com.grupoaval.payments.v1.GetFinancialEntitiesResponse;
 import com.grupoaval.payments.v1.GetFinancialEntitiesRqType;
 import com.grupoaval.xsd.ifx.AdditionalDataType;
 import com.grupoaval.xsd.ifx.CatalogueType;
+
 
 /**
  * @author diana.martinez
@@ -26,16 +39,20 @@ public class DemoClient {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// https://192.168.135.28:444/PJBA_Multiaplicacion-consultarDatosCatalogos/FinancialEntitiesInqPort
-//		String serviceUrl = "http://localhost:8084/catalog/financialEntities?WSDL";// 192.168.135.28 ---- 10.5.14.39
+		
+//		String serviceUrl = "https://localhost:443/catalog/financialEntities?WSDL";// 192.168.135.28 ---- 10.5.14.39
 		String serviceUrl = "https://192.168.135.28:442/PJBA_Multiaplicacion-consultarDatosCatalogos/FinancialEntitiesInqPort?wsdl";// 192.168.135.28 ---- 10.5.14.39
-		// String serviceUrl =
-		// "https://192.168.135.28:444/PJBA_Multiaplicacion-consultarDatosCatalogos/FinancialEntitiesInqPort?WSDL";//192.168.135.28
-		// ---- 10.5.14.39
+		String keyStorePath = "./src/main/resources/dp-sign-avaldigitallabs-villas.p12";
+		String keyStorePassword = "4v4l!2018";
 		JaxWsProxyFactoryBean factory = new JaxWsProxyFactoryBean();
 		factory.setServiceClass(FinancialEntitiesInq.class);
 		factory.setAddress(serviceUrl);
 		FinancialEntitiesInq catalogsTypesProxy = (FinancialEntitiesInq) factory.create();
+		
+		//certificado SSL
+		
+		ConfigureSSL confssl= new ConfigureSSL();
+		confssl.configureSSLOnTheClient(catalogsTypesProxy, keyStorePath, keyStorePassword);
 
 		GetFinancialEntitiesRequest getFinancialEntitiesRq = new GetFinancialEntitiesRequest();
 		GetFinancialEntitiesRqType request = new GetFinancialEntitiesRqType();
@@ -47,11 +64,6 @@ public class DemoClient {
 		catalogue.setAdditionalData(additionalData);
 		catalogue.setKey("100");
 		catalogueList.add(catalogue);
-
-		// RefInfoType refInfoType = new RefInfoType();
-		// refInfoType.setRefId("prprueba");
-		// refInfoType.setRefType("prueba");
-		// request.setRefInfo(refInfoType);
 		
 		getFinancialEntitiesRq.setGetFinancialEntitiesRq(request);
 		System.out.println("mi catalogo size:"+request.getCatalogue().size());
@@ -66,6 +78,7 @@ public class DemoClient {
 
 		}
 
-		// http://192.168.135.28:442/PJBA_Multiaplicacion-consultarDatosCatalogos/FinancialEntitiesInqPort
 	}
+	
+	
 }
